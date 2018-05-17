648f04d83194d3c2ef7db73fa26f813e40b6c8db
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-05-17 17:22:24 -0400

    first change

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View,AppRegistry } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some text</Text>
+
+  )
+}
+
+AppRegistry.registerComponent("reactflowjs",()=>App)
