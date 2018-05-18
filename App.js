f41a8df3fe6ad53dee25cba3b6fbcdc4b4bd7e1b
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-05-18 00:05:45 -0400

    can upload and display photos

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Text ,AppRegistry,View} from 'react-native';
 import Header from './src/components/header'
 import Body from './src/components/body'
+//import ClickComponent from './src/components/upload'
 export default class App extends Component {
   render() {
     return (
       <View>
        <Header title={"Artificial Intelligence App"}/>
        <Body />
+      
       </View>
       
   )
--- src/components/body.js
+++ src/components/body.js
@@ -1,28 +1,156 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {
+    ActivityIndicator,
+    
+    Clipboard,
+    Image,
+    Share,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+import Button from 'react-native-button';
 import Exponent, { Constants, ImagePicker } from 'expo';
 
 export default class Body extends Component {
-    constructor(props) {
+    constructor(props) 
+    {
         super(props)
         this.state = {
             image: null,
             uploading: false,
           };
           let { image } = this.state;
-
     }
+    _maybeRenderUploadingOverlay = () => {
+        if (this.state.uploading) {
+          return (
+            <View
+              style={[
+                StyleSheet.absoluteFill,
+                {
+                  backgroundColor: 'rgba(0,0,0,0.4)',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              <ActivityIndicator color="#fff" animating size="large" />
+            </View>
+          );
+        }
+    };
+    _maybeRenderImage = () => {
+        let { image } = this.state;
+        if (!image) {
+          return;
+        }
+    
+        return (
+          <View
+            style={{
+              marginTop: 30,
+              width: 250,
+              borderRadius: 3,
+              elevation: 2,
+              shadowColor: 'rgba(0,0,0,1)',
+              shadowOpacity: 0.2,
+              shadowOffset: { width: 4, height: 4 },
+              shadowRadius: 5,
+            }}>
+            <View
+              style={{
+                borderTopRightRadius: 3,
+                borderTopLeftRadius: 3,
+                overflow: 'hidden',
+              }}>
+              <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+            </View>
+    
+            <Text
+              onPress={this._copyToClipboard}
+              onLongPress={this._share}
+              style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+              {image}
+            </Text>
+          </View>
+        );
+    };
+    _handleImagePicked = async pickerResult => {
+        let uploadResponse, uploadResult;
+    
+        try {
+          this.setState({ uploading: true });
+    
+          if (!pickerResult.cancelled) {
+            uploadResponse = await uploadImageAsync(pickerResult.uri);
+            uploadResult = await uploadResponse.json();
+            this.setState({ image: uploadResult.location });
+          }
+        } catch (e) {
+          console.log({ uploadResponse });
+          console.log({ uploadResult });
+          console.log({ e });
+          alert('Upload failed, sorry :(');
+        } finally {
+          this.setState({ uploading: false });
+        }
+      };
+    
+    _pickImage = async () => {
+        let pickerResult = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: false,
+          aspect: [4, 3],
+        });
+        this.setState({image:pickerResult.uri})
+    
+       // this._handleImagePicked(pickerResult);
+    };
+    
   
 
   render() {
     
+    let { image } = this.state;
+    // flex styling
+    const styles  = {
+        viewStyle: {  paddingTop: 40,flex: 1, alignItems: 'center', justifyContent: 'center' },
+        textStyle: {
+            fontSize: 20,
+            marginBottom: 20,
+            textAlign: 'center',
+            marginHorizontal: 15
+        }
+
+      }
+    const {textStyle,viewStyle}= styles
+   
+    
+    
+    
 
     return (
-      <View>
-        <Text>
-          Example: Upload ImagePicker result
-        </Text>
-      </View>
+        <View>
+           <View style={viewStyle}>
+             <Text style= {textStyle}>
+                 Example: Upload ImagePicker result
+             </Text>
+            </View>
+           <View>
+           <Button onPress={this._pickImage}>
+                Pick an image from camera roll
+          </Button>
+           <Button onPress={()=>this._takePhoto()} >
+               Take a photo
+           </Button>
+           {this._maybeRenderImage()}
+           {this._maybeRenderUploadingOverlay()}
+           <StatusBar barStyle="default" />
+          </View>
+        </View>
+
+
     );
   }
 
